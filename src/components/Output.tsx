a699ae5ecb5f1e8e5264a12eb022afe6c3656059
Raju Choudhary <razoo.choudhary69@gmail.com>
2023-08-31 00:04:03 +0545

    ✅ Add Games Section

--- src/components/Output.tsx
+++ src/components/Output.tsx
@@ -8,49 +8,55 @@ import Welcome from "./commands/Welcome";
 import History from "./commands/History";
 import Projects from "./commands/Projects";
 import Socials from "./commands/Socials";
-import {OutputContainer, UsageDiv} from "./styles/Output.styled";
-import {termContext} from "./Terminal";
-import {useContext} from "react";
+import { OutputContainer, UsageDiv } from "./styles/Output.styled";
+import { termContext } from "./Terminal";
+import { useContext } from "react";
 import Work from "./commands/Work";
 import Download from "./commands/Download";
+import Games from "./commands/Games";
 
 type Props = {
-    index: number;
-    cmd: string;
+  index: number;
+  cmd: string;
 };
 
-const Output: React.FC<Props> = ({index, cmd}) => {
-    const {arg} = useContext(termContext);
+const Output: React.FC<Props> = ({ index, cmd }) => {
+  const { arg } = useContext(termContext);
 
-    const specialCmds = ["projects", "socials"];
+  const specialCmds = ["projects", "socials", "games"];
 
-    // return 'Usage: <cmd>' if command arg is not valid
-    // eg: about tt
-    if (!specialCmds.includes(cmd) && arg.length > 0)
-        return <UsageDiv data-testid="usage-output">Usage: {cmd}</UsageDiv>;
+  // return 'Usage: <cmd>' if command arg is not valid
+  // eg: about tt
+  if (!specialCmds.includes(cmd) && arg.length > 0)
+    return <UsageDiv data-testid="usage-output">Usage: {cmd}</UsageDiv>;
 
-    return (
-        <OutputContainer data-testid={index === 0 ? "latest-output" : null}>
-            {
-                {
-                    about: <About/>,
-                    clear: <Clear/>,
-                    education: <Education/>,
-                    email: <Email/>,
-                    help: <Help/>,
-                    history: <History/>,
-                    projects: <Projects/>,
-                    pwd: <GeneralOutput>/home/MrJukeman</GeneralOutput>,
-                    socials: <Socials/>,
-                    welcome: <Welcome/>,
-                    download : <Download />,
-                    whoami: <GeneralOutput>Well, it looks like you're having an identity crisis even the terminal can't
-                        solve. Maybe try asking a mirror instead? 🪞</GeneralOutput>,
-                    work: <Work/>
-                }[cmd]
-            }
-        </OutputContainer>
-    );
+  return (
+    <OutputContainer data-testid={index === 0 ? "latest-output" : null}>
+      {
+        {
+          about: <About />,
+          clear: <Clear />,
+          education: <Education />,
+          email: <Email />,
+          help: <Help />,
+          history: <History />,
+          projects: <Projects />,
+          pwd: <GeneralOutput>/home/MrJukeman</GeneralOutput>,
+          socials: <Socials />,
+          welcome: <Welcome />,
+          download: <Download />,
+          games: <Games />,
+          whoami: (
+            <GeneralOutput>
+              Well, it looks like you're having an identity crisis even the
+              terminal can't solve. Maybe try asking a mirror instead? 🪞
+            </GeneralOutput>
+          ),
+          work: <Work />,
+        }[cmd]
+      }
+    </OutputContainer>
+  );
 };
 
 export default Output;
---
+++ src/components/commands/Games.tsx
@@ -0,0 +1,58 @@
+import { useContext, useEffect } from "react";
+import {
+  checkRedirect,
+  getCurrentCmdArry,
+  isArgInvalid,
+} from "../../utils/funcs";
+import {
+  ProjectContainer,
+  ProjectDesc,
+  ProjectTitle,
+} from "../styles/Projects.styled";
+import { termContext } from "../Terminal";
+import Usage from "../Usage";
+
+const Games: React.FC = () => {
+  const { arg, history, rerender } = useContext(termContext);
+
+  /* ===== get current command ===== */
+  const currentCommand = getCurrentCmdArry(history);
+
+  /* ===== check current command is redirect ===== */
+  useEffect(() => {
+    if (checkRedirect(rerender, currentCommand, "games")) {
+      Game.forEach(({ id, url }) => {
+        id === parseInt(arg[1]) && window.open(url, "_blank");
+      });
+    }
+  }, [arg, rerender, currentCommand]);
+
+  /* ===== check arg is valid ===== */
+  const checkArg = () =>
+    isArgInvalid(arg, "go", ["1"]) ? <Usage cmd="games" /> : null;
+
+  return arg.length > 0 || arg.length > 2 ? (
+    checkArg()
+  ) : (
+    <div data-testid="games">
+      {Game.map(({ id, title, desc }) => (
+        <ProjectContainer key={id}>
+          <ProjectTitle>{`${id}. ${title}`}</ProjectTitle>
+          <ProjectDesc>{desc}</ProjectDesc>
+        </ProjectContainer>
+      ))}
+      <Usage cmd="games" marginY />
+    </div>
+  );
+};
+
+const Game = [
+  {
+    id: 1,
+    title: "Tower Block",
+    desc: "A javascript based Tower Building Block Games",
+    url: "https://projects.rajuchoudhary.com.np/",
+  },
+];
+
+export default Games;
